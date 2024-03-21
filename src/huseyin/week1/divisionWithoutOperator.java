6136250036dfe9364fe2e394aea6d2e0d2f9cbc1
zanarditgs <turkishairlinescrew@gmail.com>
2024-03-21 13:32:35 -0400

    week1 solutions

---
+++ src/huseyin/week1/divisionWithoutOperator.java
@@ -0,0 +1,13 @@
+package huseyin.week1;
+
+public class divisionWithoutOperator {
+    public static void main(String[] args) {
+        //Write a method that can divide two numbers without using division operator
+
+
+    }
+
+
+
+
+}
