e4a27fddd9d24c97dc69b271dc4fe0eda8f2ab7a
kirpimarmot <cevikkursat@gmail.com>
2024-03-21 17:56:25 +0100

    week1 tasks added

---
+++ src/week1/divisionWithoutOperator.java
@@ -0,0 +1,13 @@
+package week1;
+
+public class divisionWithoutOperator {
+    public static void main(String[] args) {
+        //Write a method that can divide two numbers without using division operator
+
+
+    }
+
+
+
+
+}
