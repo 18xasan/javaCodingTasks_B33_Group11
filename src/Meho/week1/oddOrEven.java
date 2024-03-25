3b87507f5803ebc3c1fd2d4850f517618f1a80d5
Meho Odobasic <mfodobasic@gmail.com>
2024-03-25 14:31:30 -0400

    oddOrEven solution done

--- src/Meho/week1/oddOrEven.java
+++ src/Meho/week1/oddOrEven.java
@@ -2,9 +2,19 @@ package Meho.week1;
 
 public class oddOrEven {
     public static void main(String[] args) {
-        //Write  a method which can identifies given number is even or odd
+
+        int num = 10;
+        String result = "isEvenOrEven";
+        if (num % 2 == 0) {
+            result = "isEven";
+        } else {
+            result = "isOdd";
+        }
+        System.out.println(result);
 
 
     }
-
 }
+/*
+Write  a method which can identifies given number is even or odd
+ */
